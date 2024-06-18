0e34fe843540dd79b3611f546722f563de797658
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2024-06-18 17:16:33 +0300

    try google map

--- app/src/main/java/com/abapps/changetheme/ui/main/MainActivity.kt
+++ app/src/main/java/com/abapps/changetheme/ui/main/MainActivity.kt
@@ -1,61 +1,112 @@
 package com.abapps.changetheme.ui.main
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import com.abapps.changetheme.resource.AppTheme
-import com.abapps.changetheme.resource.LocalThemeIsDark
-import com.abapps.changetheme.resource.Resources
-import com.abapps.changetheme.util.LanguageCode
+import com.abapps.changetheme.ui.map.MapsUiState
+import com.abapps.changetheme.ui.map.MapsViewModel
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.rememberMarkerState
 
 class MainActivity : ComponentActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val viewModel: MapsViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
+
         super.onCreate(savedInstanceState)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         setContent {
             val mainViewModel by viewModels<MainViewModel>()
             val languageCode by mainViewModel.state.collectAsState()
             AppTheme(languageCode = languageCode) {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    var isDark by LocalThemeIsDark.current
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(innerPadding),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.spacedBy(16.dp)
-                    ) {
-                        Button(onClick = { isDark = !isDark }) {
-                            Text(text = if (isDark) "Light" else "Dark")
-                        }
-                        Button(onClick = {
-                            mainViewModel.changeLanguage(
-                                if (languageCode == LanguageCode.EN) LanguageCode.AR.value
-                                else LanguageCode.EN.value
-                            )
-                        }
-                        ) {
-                            Text(text = Resources.strings.hello)
-                        }
-                    }
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    RequestLocationPermission { getLastKnownLocation() }
+                    val state by viewModel.uiState.collectAsState()
+                    MapView(state)
                 }
             }
+        }
+    }
+
+    private fun getLastKnownLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                location?.let {
+                    viewModel.updateLocation(it)
+                    viewModel.fetchNearbyRestaurants(it)
+                }
+            }
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun RequestLocationPermission(onPermissionGranted: () -> Unit) {
+    val locationPermissionState = rememberPermissionState(Manifest.permission.ACCESS_FINE_LOCATION)
+
+    if (locationPermissionState.status.isGranted) {
+        onPermissionGranted()
+    } else {
+        LaunchedEffect(locationPermissionState) {
+            locationPermissionState.launchPermissionRequest()
+        }
+    }
+}
+
+@Composable
+fun MapView(state: MapsUiState) {
+    val cameraPositionState = rememberCameraPositionState {
+        state.currentLocation?.let {
+            position = CameraPosition.fromLatLngZoom(LatLng(it.latitude, it.longitude), 15f)
+        }
+    }
+
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+        state.restaurants.forEach { restaurant ->
+            Marker(
+                state = rememberMarkerState(position = LatLng(restaurant.lat, restaurant.lng)),
+                title = restaurant.name
+            )
         }
     }
 }
